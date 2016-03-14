5d830877138e86bec54f339105afd5023075e7d3
veeweeherman <vy.hoang.cu@gmail.com>
2016-03-13 18:10:34 -0700

    added more routes for diff data displays

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var Data = require('./dummyData/sampleData.js');
+var Data = require('./dummyData/sampleData.js').data;
 var _ = require('underscore');
 
 
@@ -23,31 +23,85 @@ router.get('/', function(req,res){
   res.json({ message: 'API is up and running, oi oi oi!!'})
 });
 
-
-router.route('/items')
+// TEST ROUTE
+router.route('/test')
   .get(function(req, res){
+    var foo = [{name: 'moe', age: 10}, {name: 'larry', age: 50}, {name: 'curly', age: 6}, {name: 'zed', age: 1}];
+    // var baz = _.sortBy(foo, 'age');
 
-    // var foo = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}, {name: 'zed', age: 3}];
-    // var baz = _.sortBy(foo, 'name');
+    //  works the same as:
 
-    var things = Data.data;
-    var sortedByISO = _.sortBy(things, 'createdAt');
+    // var baz = _.sortBy(foo, function(prop){
+    //   if (prop.age){
+    //     return prop.age;
+    //     // return prop.age * -1 //--> to return in descending order for numbers
+    //   }
+    // })
 
-      res.json(sortedByISO);
+    var baz = _.sortBy(foo, function(prop){
+      if (prop.name){
+        return prop.name;
+        // return prop.name.charCodeAt() * -1; //--> to return in descending alphabetical order
+      }
+    })
+
+    res.json(baz);
 
   })
+
+// ALL ROUTES
+
+// SORT ITEMS BY DATE CREATED
+router.route('/sortItemsByDateAsc')
+  .get(function(req, res){
+    var sortedByISO = _.sortBy(Data, 'createdAt');
+    res.json(sortedByISO);
+
+  })
+
+router.route('/sortItemsByDateDesc')
+  .get(function(req, res){
+    // var datesOnly = _.map(Data, function(value, index, list){
+    //   return value.createdAt;
+    // })
+    // var datesOnlyDesc = datesOnly.reverse();
+    // var stringDates = ["2014-08-25T19:33:21.153Z","2014-08-25T22:33:26.282Z","2014-08-26T01:25:48.754Z","2014-08-26T17:47:22.885Z","2014-08-26T18:19:05.321Z","2014-08-26T20:46:06.044Z","2014-08-26T23:00:18.800Z","2014-08-26T23:54:48.754Z","2014-08-27T17:18:55.068Z","2014-08-25T19:31:50.180Z"].sort().reverse();
+    // is there a beter way to sort alphabetically backwards without having to use .reverse()? time complexity adds one more iteration with .reverse()
+
+    var sortedByISO = _.sortBy(Data, 'createdAt');
+    var sortedByISODesc = sortedByISO.reverse();
+    res.json(sortedByISODesc);
+
+  })
+
+// SORT ITEM BY PRICE
+router.route('/priceAsc')
+  .get(function(req, res){
+    var priceAsc = _.sortBy(Data, 'price');
+    res.json(priceAsc);
+  })
+
+router.route('/priceDesc')
+  .get(function(req, res){
+    var priceDesc = _.sortBy(Data, function(item){
+      return item.price * -1;
+    });
+    res.json(priceDesc);
+  })
+
+// DISPLAY ITEM BY USER ID
+
+router.route('/:userid')
+
+
+
 
 app.use('/api', router);
 
   // lsof -i TCP:8080
   // kill 27165 <-- # is PID number
 
-
-
-
-
-
-
-
+// var nums = [2,1,3,10]
+// var sortedNums = _.sortBy(nums, function(num){ return Math.sin(num);})
 app.listen(port);
 console.log('it\'s showtime!');
